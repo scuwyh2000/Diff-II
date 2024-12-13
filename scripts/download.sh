b5f2234361318d62de81bf5a1a971bc1efb188cc
Yanghao Wang <87008857+scuwyh2000@users.noreply.github.com>
2024-12-13 23:41:53 +0800

    Add files via upload

---
+++ scripts/download.sh
@@ -0,0 +1,9 @@
+wget https://huggingface.co/datasets/Luka-Wang/Diff-II/resolve/main/datasets.zip
+wget https://huggingface.co/datasets/Luka-Wang/Diff-II/resolve/main/ft_ti_db.zip
+wget https://huggingface.co/datasets/Luka-Wang/Diff-II/resolve/main/suffix.zip
+unzip datasets.zip
+unzip ft_ti_db.zip
+unzip suffix.zip
+rm -rf ft_ti_db.zip
+rm -rf suffix.zip
+rm -rf datasets.zip
